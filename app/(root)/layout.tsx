cf7d933a10b26df0456196a01bea2333b2bc7c9c
Adrian <adrian.hassan.ef@gmail.com>
2025-02-25 19:31:59 +0100

    Start

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,12 @@
+import Navbar from "../components/Navbar";
+
+const RootLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
+  return (
+    <main className="font-work-sans">
+      <Navbar />
+      {children}
+    </main>
+  );
+};
+
+export default RootLayout;
