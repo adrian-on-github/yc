213eafac60b8cbdc68a5d18ae4eb93e984270d80
Adrian <adrian.hassan.ef@gmail.com>
2025-02-26 15:07:14 +0100

    hero section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,12 @@
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl">Home</h1>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Startup, <br /> Connect with Entrepreneurs
+        </h1>
+        <p className="sub-heading !max-w-3xl"></p>
+      </section>
     </>
   );
 }
