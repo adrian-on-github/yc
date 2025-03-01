b0be9ec3e5764a2ed03948290e5cd71fea0d9a78
Adrian <adrian.hassan.ef@gmail.com>
2025-03-01 13:13:25 +0100

    no font anymore

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,22 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import localFont from "next/font/local";
+import localFont from "next/font";
 import "easymde/dist/easymde.min.css";
 import { Toaster } from "sonner";
 
-const workSans = localFont({
-  src: [
-    { path: "./fonts/WorkSans-Black.ttf", weight: "900", style: "normal" },
-    { path: "./fonts/WorkSans-Extrabold.ttf", weight: "800", style: "normal" },
-    { path: "./fonts/WorkSans-Bold.ttf", weight: "700", style: "normal" },
-    { path: "./fonts/WorkSans-Semibold.ttf", weight: "600", style: "normal" },
-    { path: "./fonts/WorkSans-Medium.ttf", weight: "500", style: "normal" },
-    { path: "./fonts/WorkSans-Regular.ttf", weight: "400", style: "normal" },
-    { path: "./fonts/WorkSans-Thin.ttf", weight: "300", style: "normal" },
-    { path: "./fonts/WorkSans-Extralight.ttf", weight: "200", style: "normal" },
-  ],
-  variable: "--font-work-sans",
-});
+// const workSans = localFont({
+//   src: [
+//     { path: "./fonts/WorkSans-Black.ttf", weight: "900", style: "normal" },
+//     { path: "./fonts/WorkSans-Extrabold.ttf", weight: "800", style: "normal" },
+//     { path: "./fonts/WorkSans-Bold.ttf", weight: "700", style: "normal" },
+//     { path: "./fonts/WorkSans-Semibold.ttf", weight: "600", style: "normal" },
+//     { path: "./fonts/WorkSans-Medium.ttf", weight: "500", style: "normal" },
+//     { path: "./fonts/WorkSans-Regular.ttf", weight: "400", style: "normal" },
+//     { path: "./fonts/WorkSans-Thin.ttf", weight: "300", style: "normal" },
+//     { path: "./fonts/WorkSans-Extralight.ttf", weight: "200", style: "normal" },
+//   ],
+//   variable: "--font-work-sans",
+// });
 
 export const metadata: Metadata = {
   title: "YC Directory",
@@ -30,7 +30,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={workSans.variable}>
+      <body>
         {children} <Toaster />
       </body>
     </html>
